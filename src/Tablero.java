07d03bf9ade2f71a445cf04152e3c2c48a11982e
Rolan Salas <rolan.salas02@gmail.com>
2020-02-23 23:01:31 -0400

    Rompecabezas Terminado

--- src/Tablero.java
+++ src/Tablero.java
@@ -10,44 +10,79 @@ import java.util.ArrayList;
  *
  * @author rolando mamani salas
  */
-public class Tablero {
+public final class Tablero {
 
     Ficha[][] tablero;
-    private int tamano;
+    private int numeroFichas;
     Ficha ficha;
     ArrayList<String> historial = new ArrayList<String>();
 
+    //OBJETIVO 1 
+    /*   Como primer objetivo debemos generar la siguiente estructura:
+            1 2 3 4
+            5 6 7 8
+            9 10 11 12
+            13 14 15 0
+            Figura 1.
+        El caracter 0 representa el huequito. Cada fila está representada por una lista.
+        Adicionalmente se necesita un elemento que permita definir la posición del huequito. Escribe el
+        código necesario para representar dicha estructura.
+        Para comprobar que la estructura inicial es la apropiada, se necesita crear un método que devuelva
+        constantemente el tablero ordenado y formateado, como se muestra en la figura 1.
+     */
     public Tablero(int n) {
         if (verificarMatrizPerfecta(n)) {
-            tamano = (int) Math.sqrt(n);
-            armarTaleroRompecabeza();
+            numeroFichas = (int) Math.sqrt(n);
+            armarTalero();
         }
     }
 
-    public void armarTaleroRompecabeza() {
-        tablero = new Ficha[tamano][tamano];
+    public void armarTalero() {
+        tablero = new Ficha[numeroFichas][numeroFichas];
         int index = 1;
-        for (int x = 0; x < tamano; x++) {
-            for (int y = 0; y < tamano; y++) {
+        for (int x = 0; x < numeroFichas; x++) {
+            for (int y = 0; y < numeroFichas; y++) {
                 tablero[x][y] = new Ficha(x, y, index);
                 index++;
             }
         }
     }
 
-    public String posiblesMovimientosFicha0() {
-        String res = "";
-        Ficha[] movimientos = mostrarFichasVecinas(ficha);
-        for (Ficha p : movimientos) {
-            if (p != null) {
-                res += p.toStringPosicion() + "\n";
-            }
-        }
-        return res;
+    public boolean verificarMatrizPerfecta(int n) {
+        double sq = Math.sqrt(n);
+        return ((sq - Math.floor(sq)) == 0);
     }
 
-    public String[] posiblesMovimientosString() {
-        String[] res;
+    /*Objetivo 2. Identificar Posibles Movidas
+        Cuáles son las posiciones donde se puede mover el huequito?
+        Dada una posición inicial del huequito, crearemos un método que permita identificar las posibles
+        posiciones, en las cuales puede terminar el huequito, considerando que puede moverse una
+        posición a la vez.
+        Por ejemplo, en el siguiente tablero, el huequito está en la fila 0, columna 1:
+                1 0 3 4
+                5 6 7 8
+                9 10 11 12
+                13 14 15 16
+        Las posibles movidas serían:
+                1. fila 0, columna 0
+                2. fila 0, columna 2
+                3. fila 1, columna 1
+        Eso significa que el método debería devolver una colección de posiciones.
+    
+     */
+    public String posiblesMovimientosFicha0() {                       //Ejemplo de clase  public ArrayList identificarMovidasDelHuequito(){
+        String resultado = "";                                          //            calculas las posibles movidas del huquito
+        Ficha[] movimientos = mostrarFichasVecinas(ficha);              //             cada posible movida se anade al array resultado
+        for (Ficha p : movimientos) {
+            if (p != null) {
+                resultado += p.toStringPosicion() + "\n";
+            }
+        }
+        return resultado;
+    }
+
+    public String[] posiblesMovimientosString() {                    // Cambiar este metodo 
+        String[] resultado;
         int index = 0;
         Ficha[] movimientos = mostrarFichasVecinas(ficha);
         for (Ficha p : movimientos) {
@@ -55,50 +90,54 @@ public class Tablero {
                 index++;
             }
         }
-        res = new String[index];
+        resultado = new String[index];
         index = 0;
         for (int i = 0; i < 4; i++) {
             if (movimientos[i] != null) {
-                res[index] = direccionString(i);
+                resultado[index] = direccionString(i);
                 index++;
             }
         }
-        return res;
+        return resultado;
     }
 
-    public void realizarMovimiento(String Direccion) {
+    /* Objetivo 3. Aplicar una Movida
+        Dado una lista de posibles movidas y la posición del huequito, se requiere aplicar una movida
+        cualquiera, de modo que el resultado es el tablero con la movida aplicada.
+        Debes tener en cuenta que al aplicar una movida, no se debe salir del tablero.
+     */
+    public void realizarMovimiento(String direccion) {
         Ficha[] movimientosLegales = mostrarFichasVecinas(ficha);
-        switch (Direccion) {
+        switch (direccion) {
             case "Arriba":
                 if (movimientosLegales[0] != null) {
                     intercambiarFichas(movimientosLegales[0], ficha);
-                    historial.add(Direccion);
+                    historial.add(direccion);
                 }
                 break;
             case "Izquierda":
                 if (movimientosLegales[1] != null) {
                     intercambiarFichas(movimientosLegales[1], ficha);
-                    historial.add(Direccion);
+                    historial.add(direccion);
                 }
                 break;
             case "Derecha":
                 if (movimientosLegales[2] != null) {
                     intercambiarFichas(movimientosLegales[2], ficha);
-                    historial.add(Direccion);
+                    historial.add(direccion);
                 }
                 break;
             case "Abajo":
                 if (movimientosLegales[3] != null) {
                     intercambiarFichas(movimientosLegales[3], ficha);
-                    historial.add(Direccion);
+                    historial.add(direccion);
                 }
                 break;
         }
-
     }
 
     public void ingresarFicha(Ficha ingreso) {
-        tablero[ingreso.getCoordenadaX()][ingreso.getCoordenadaY()] = ingreso;
+        ingreso = tablero[ingreso.getCoordenadaX()][ingreso.getCoordenadaY()];
         if (ingreso.getValor() == 0) {
             ficha = ingreso;
         }
@@ -131,24 +170,19 @@ public class Tablero {
         return movimientos;
     }
 
-    public boolean movimientoValido(int x, int y) {
-        if (x < 0 || x >= tamano) {
+    public boolean movimientoValido(int cordenadaX, int cordenadaY) {
+        if (cordenadaX < 0 || cordenadaX >= numeroFichas) {
             return false;
         }
-        if (y < 0 || y >= tamano) {
+        if (cordenadaY < 0 || cordenadaY >= numeroFichas) {
             return false;
         }
         return true;
     }
 
-    public boolean verificarMatrizPerfecta(int n) {
-        double sq = Math.sqrt(n);
-        return ((sq - Math.floor(sq)) == 0);
-    }
-
-    public String direccionString(int n) {
+    public String direccionString(int direccion) {
         String resultado = "";
-        switch (n) {
+        switch (direccion) {
             case 0:
                 resultado = "Arriba";
                 break;
@@ -165,21 +199,58 @@ public class Tablero {
         return resultado;
     }
 
+    //Formato
     @Override
     public String toString() {
         String espacio = " ";
-        String res = "";
-        for (int x = 0; x < tamano; x++) {
-            for (int y = 0; y < tamano; y++) {
+        String resultado = "";
+        for (int x = 0; x < numeroFichas; x++) {
+            for (int y = 0; y < numeroFichas; y++) {
                 Ficha temp = tablero[x][y];
                 if (temp.getValor() < 10) {
-                    res += espacio;
+                    resultado += espacio;
                 }
-                res += "" + temp.getValor();
-                res += espacio;
+                resultado += "" + temp.getValor();
+                resultado += espacio;
             }
-            res += "\n";
+            resultado += "\n";
         }
-        return res;
+        return resultado;
     }
+
+    /*Objetivo 4
+        -- Desarmar tablero de mandera aleatoria
+        -- Crear un metodo que permita generar una lista de movidas aleatorias 
+            el resultado de este metodo debe ser una lista de movidas 
+            que se apliquen al tablero
+        --Los parametros son cantidas de movidas y posicion inicial
+    
+    public DevolverLista movidasaleatorias(cantidasMovidas , posicionInicial)
+        Lista lisraDeMovidasAleatorias = new Array.......
+        lista
+        return lisraDeMovidasAleatorias;
+     */
+    public String[] MovidasAleatorias(int cantidadMovimientos, Ficha posicionInicial) {
+        String[] movidas = new String[cantidadMovimientos];
+        ingresarFicha(posicionInicial);
+        Ficha[] vecinos = mostrarFichasVecinas(ficha);
+        int posicionAleatoria;
+        for (int i = 0; i < cantidadMovimientos; i++) {
+            Ficha temporal;
+            do {
+                posicionAleatoria = numeroAleatorio(0, 16);
+                temporal = vecinos[posicionAleatoria];
+            } while (temporal == null);
+            String direccion = direccionString(posicionAleatoria);
+            movidas[i] = direccion;
+            vecinos = mostrarFichasVecinas(temporal);
+        }
+        return movidas;
+    }
+    //castear este metodo por que math debuelve double
+    public int numeroAleatorio(int numeroMinimo, int numeroMaximo) {
+        int numeroValido = (numeroMaximo - numeroMinimo) + 1;
+        return (int) (Math.random() * numeroValido) + numeroMinimo;
+    }
+
 }
---
+++ test/TableroTest.java
@@ -0,0 +1,68 @@
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author roland
+ */
+public class TableroTest {
+////    @Test
+////    public void creacionDelTableroTest() {
+////        Tablero tablero = new Tablero(15);
+////        String tableroEsperado = " 1  2  3  4 \n 5  6  7  8 \n 9 10 11 12\n 13 14 15  ";
+////        String tableroObtenido = tablero.toString();
+////        assertEquals(tableroEsperado , tableroObtenido);
+////
+////    }
+
+    @Test
+    public void verificarMatrizPerfectaTest() {
+        Tablero tablero = new Tablero(15);
+        boolean perfecta = true;
+        boolean obtenida = tablero.verificarMatrizPerfecta(25);
+        assertEquals(perfecta, obtenida);
+    }
+
+    @Test
+    public void verificarMatrizNoPerfectaTest() {
+        Tablero tablero = new Tablero(15);
+        boolean perfecta = false;
+        boolean obtenida = tablero.verificarMatrizPerfecta(24);
+        assertEquals(perfecta, obtenida);
+    }
+
+    @Test
+    public void movimientoArribaTest() {
+        Tablero tablero = new Tablero(15);
+        String esperado = "Arriba";
+        String obtenido = tablero.direccionString(0);
+        assertEquals(esperado, obtenido);
+    }
+    @Test
+    public void movimientoIzquierdaTest() {
+        Tablero tablero = new Tablero(15);
+        String esperado = "Izquierda";
+        String obtenido = tablero.direccionString(1);
+        assertEquals(esperado, obtenido);
+    }
+    @Test
+    public void movimientoDerechaTest() {
+        Tablero tablero = new Tablero(15);
+        String esperado = "Derecha";
+        String obtenido = tablero.direccionString(2);
+        assertEquals(esperado, obtenido);
+    }
+    @Test
+    public void movimientoAbajoTest() {
+        Tablero tablero = new Tablero(15);
+        String esperado = "Abajo";
+        String obtenido = tablero.direccionString(3);
+        assertEquals(esperado, obtenido);
+    }
+}
